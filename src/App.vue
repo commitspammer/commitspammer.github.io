1e3f2a143ad9326cafcb6795207c1b1fde12e7ef
commitspammer <gusta.silva.nicke@gmail.com>
2026-06-27 18:27:49 -0300

    migrate to astro project

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import { /*RouterLink,*/ RouterView } from 'vue-router'
-//import BackGlobal from './components/BackGlobal.vue'
-</script>
-
-<template>
-  <!--<header><nav><RouterLink to="/">Home</RouterLink></nav></header>-->
-  <RouterView />
-</template>
